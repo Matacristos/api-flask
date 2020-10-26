d8ca3168ddbc0a9fdc57a7aba302e5e71e7333fb
davidlorca <lorcacamposdavid@gmail.com>
2020-10-26 10:58:50 +0100

    Graph endpoint

--- init.py
+++ init.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, send_file
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Working!"
+
+
+@app.route("/getGraph", methods=['GET'])
+def get_graph():
+    return send_file('images/graph.png', mimetype='image/png')
+
 
 if __name__ == "__main__":
     app.run()
